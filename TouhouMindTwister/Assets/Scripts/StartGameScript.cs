4b537f7aec18002b75cd58dc964962ae8ce86180
Xiu Guang <170025H@mymail.nyp.edu.sg>
2018-09-16 20:31:41 +0800

    Added startgamescript for mainmenu to gamemodeselection navigation

---
+++ TouhouMindTwister/Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGameScript : MonoBehaviour {
+    public RectTransform Gamemode;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PressedGamemode()
+    {
+        Gamemode.gameObject.SetActive(true);
+    }
+
+    public void ExitGamemode()
+    {
+        Gamemode.gameObject.SetActive(false);
+    }
+}
